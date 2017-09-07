4e3611da253f7a189ee45e93aafcc2a376993c6c
Eric Murphy <emurphy@Eric-Murphy.local>
2017-09-07 15:45:20 -0400

    fix

--- mobile/backend/application.js
+++ mobile/backend/application.js
@@ -14,10 +14,10 @@ app.use(cors()); // Enable CORS for all requests
 var jsonParser = bodyParser.json(); // Setup JSON body parser
 
 app.post('/', function (req, res) {
-	res.status(200).end()
-  });
+	res.status(200).end();
+});
 
-require('./routes/routes')(app, jsonParser, upload); // Bootstrap app routes
+require('./config/routes')(app, jsonParser, upload); // Bootstrap app routes
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 7001;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
